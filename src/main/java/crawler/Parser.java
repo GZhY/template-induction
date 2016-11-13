fba8fdbd52bc0c21360b04038f21a3a3ec831683
GZhY <gaoz_y@163.com>
2016-11-13 22:25:54 +0800

    add Parser

--- src/main/java/crawler/Parser.java
+++ src/main/java/crawler/Parser.java
@@ -1,5 +1,7 @@
 package crawler;
 
+import org.w3c.dom.Document;
+
 /**
  * This is a XXX class.
  * What is the function of this class?
@@ -8,4 +10,12 @@ package crawler;
  * @version 1.0
  */
 public class Parser {
+
+    public static Document url2Document(String url){
+
+    }
+
+    public static Document html2Document(String html){
+
+    }
 }
