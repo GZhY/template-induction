873830e610c19778df5b8c6ce9fc13f1f5f1a89e
GZhY <gaoz_y@163.com>
2016-11-13 23:56:32 +0800

    add methods of Parser class

--- src/main/java/crawler/Parser.java
+++ src/main/java/crawler/Parser.java
@@ -1,6 +1,11 @@
 package crawler;
 
-import org.w3c.dom.Document;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This is a XXX class.
@@ -11,11 +16,32 @@ import org.w3c.dom.Document;
  */
 public class Parser {
 
-    public static Document url2Document(String url){
-
+    public static Document url2Document(String url) {
+        try {
+            return Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36")
+                    .timeout(5000).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static Document html2Document(String html){
-
+    public static Document html2Document(String html, String baseUri) {
+        return Jsoup.parse(html, baseUri);
     }
+
+    public static Document html2Document(String html) {
+        return Jsoup.parse(html);
+    }
+
+    public static Document filePath2Document(String filePath, String baseUri){
+        try {
+            return Jsoup.parse(new File(filePath), "UTF-8", baseUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
