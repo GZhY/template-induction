6edd7722d07f0143334bbd28c3cf0cce96a8223a
GZhY <gaoz_y@163.com>
2016-11-15 18:10:11 +0800

    add  ExtractXpath

---
+++ src/main/java/templateGenerating/extractor/ExtractXPath.java
@@ -0,0 +1,18 @@
+package templateGenerating.extractor;
+
+import org.w3c.dom.Node;
+
+/**
+ * This is a XXX class.
+ * What is the function of this class?
+ *
+ * @author GZhY
+ * @version 1.0
+ */
+public class ExtractXPath extends ExtractTemplate{
+
+    @Override
+    public String getTemplate(Node n) {
+        return null;// TODO: 2016/11/15
+    }
+}
