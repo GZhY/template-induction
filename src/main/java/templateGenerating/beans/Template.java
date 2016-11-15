56ca78293d07f27fadc62e498b15742517f65cdb
GZhY <gaoz_y@163.com>
2016-11-15 17:24:23 +0800

    add Template

--- src/main/java/templateGenerating/beans/Template.java
+++ src/main/java/templateGenerating/beans/Template.java
@@ -1,5 +1,7 @@
 package templateGenerating.beans;
 
+import java.util.List;
+
 /**
  * This is a XXX class.
  * What is the function of this class?
@@ -9,4 +11,29 @@ package templateGenerating.beans;
  */
 public class Template {
     TemplateType type;
+    List<String> expressions;
+
+    public TemplateType getType() {
+        return type;
+    }
+
+    public void setType(TemplateType type) {
+        this.type = type;
+    }
+
+    public List<String> getExpressions() {
+        return expressions;
+    }
+
+    public void setExpressions(List<String> expressions) {
+        this.expressions = expressions;
+    }
+
+    @Override
+    public String toString() {
+        return "Template{" +
+                "type=" + type +
+                ", expressions=" + expressions +
+                '}';
+    }
 }
