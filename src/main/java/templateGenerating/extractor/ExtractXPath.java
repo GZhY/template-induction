d51acef389d649af565f559cd55d0b6844e7caff
GZhY <gaoz_y@163.com>
2016-11-16 15:32:39 +0800

    change ExtractXPath

--- src/main/java/templateGenerating/extractor/ExtractXPath.java
+++ src/main/java/templateGenerating/extractor/ExtractXPath.java
@@ -1,6 +1,10 @@
 package templateGenerating.extractor;
 
-import org.w3c.dom.Node;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Node;
+
+import java.io.File;
 
 /**
  * This is a XXX class.
@@ -11,8 +15,20 @@ import org.w3c.dom.Node;
  */
 public class ExtractXPath extends ExtractTemplate{
 
+    public ExtractXPath(String html2Extract) {
+        super(html2Extract);
+    }
+
+    public ExtractXPath(File file2Extract) {
+        super(file2Extract);
+    }
+
+    public ExtractXPath(Document document4Jsoup) {
+        super(document4Jsoup);
+    }
+
     @Override
-    public String getTemplate(Node n) {
+    protected String getTemplate(final Node n) {
         return null;// TODO: 2016/11/15
     }
 }
